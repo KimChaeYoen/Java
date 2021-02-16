640ae142c0ac22c2beee32b667f068e4e02775b1
KimChaeYeon <kcy1060@naver.com>
2021-02-16 16:57:16 +0900

    Exam05까지 커밋

--- src/ch02/Exam01.java
+++ src/ch02/Exam01.java
@@ -10,10 +10,18 @@ public class Exam01 {
 		int var3 = 1;
 		long var4 = 1;
 		
-		//실수 타입의 변수 저장과 값 저장
-		float var5 = 2.5f;
-		double var6 = 2.5;
+		//문자 타입의 변수 선언과 값 저장
+		char var9 = 65;
+		char var10 = '한';
+		System.out.println("var9: " + var9);
+		System.out.println("var10: " + var10);
 		
+		//실수 타입의 변수 저장과 값 저장
+		float var5 = 0.123456789123456789f;
+		double var6 = 0.123456789123456789;//요놈이! float보다 정밀도가 높음~
+		System.out.println("var5: " + var5);
+		System.out.println("var5: " + var6);
+
 		//논리 타입의 변수 선언과 값 저장
 		boolean var7 = true;
 		boolean var8 = false;
