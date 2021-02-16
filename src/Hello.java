5e503f7b61012436cb78aaa1a217e77e9053dbea
KimChaeYeon <kcy1060@naver.com>
2021-02-16 14:45:26 +0900

    수정2

--- src/Hello.java
+++ src/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("Hello Java");
 		System.out.println("Hello Java");
 		System.out.println("Hello Java");
-		System.out.println("test");
+		System.out.println("test");//Å×½ºÆ®
 	}
 
 }
