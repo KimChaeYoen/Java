65223fd43439f05c2d535f12adcb91beeb569a7e
KimChaeYeon <kcy1060@naver.com>
2021-02-16 15:39:22 +0900

    ch02생성

---
+++ src/ch02/Exam01.java
@@ -0,0 +1,23 @@
+package ch02;
+
+public class Exam01 {
+
+	public static void main(String[] args) {
+		
+		//정수 타입의 변수 선언과 값 저장
+		byte var1 = 100;//-128~127
+		short var2 = 1;
+		int var3 = 1;
+		long var4 = 1;
+		
+		//실수 타입의 변수 저장과 값 저장
+		float var5 = 2.5f;
+		double var6 = 2.5;
+		
+		//논리 타입의 변수 선언과 값 저장
+		boolean var7 = true;
+		boolean var8 = false;
+		
+	}
+
+}
